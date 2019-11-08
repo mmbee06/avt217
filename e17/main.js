79186cc0bab6345c0885ac05fb708baf5a2c35b5
mmbee06 <54602972+mmbee06@users.noreply.github.com>
2019-11-07 19:05:23 -0500

    e17

---
+++ e17/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+  $("p").click(function(){
+    $(this).hide();
+  });
+  $( ".content1" )
+    .filter( ":odd" )
+      .hide()
+    .end()
+    .filter( ":even" )
+      .hover(function() {
+        $( this )
+          .toggleClass( "active" )
+          .next()
+            .stop( true, true )
+            .slideToggle();
+      });
+  $( ".content2" )
+    .filter( ":odd" )
+      .hide()
+    .end()
+    .filter( ":even" )
+      .hover(function() {
+        $( this )
+          .toggleClass( "active" )
+          .next()
+            .stop( true, true )
+            .slideToggle();
+      });
+      $( ".content3" )
+        .filter( ":odd" )
+          .hide()
+        .end()
+        .filter( ":even" )
+          .hover(function() {
+            $( this )
+              .toggleClass( "active" )
+              .next()
+                .stop( true, true )
+                .slideToggle();
+          });
+          $( ".content4" )
+            .filter( ":odd" )
+              .hide()
+            .end()
+            .filter( ":even" )
+              .hover(function() {
+                $( this )
+                  .toggleClass( "active" )
+                  .next()
+                    .stop( true, true )
+                    .slideToggle();
+              });
+              $( ".content5" )
+                .filter( ":odd" )
+                  .hide()
+                .end()
+                .filter( ":even" )
+                  .hover(function() {
+                    $( this )
+                      .toggleClass( "active" )
+                      .next()
+                        .stop( true, true )
+                        .slideToggle();
+                  });
+});
