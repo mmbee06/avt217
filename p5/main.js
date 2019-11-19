afcbdd1c730e4042083cef8a06e717129ab9baf5
mmbee06 <54602972+mmbee06@users.noreply.github.com>
2019-11-19 18:25:25 -0500

    p5

---
+++ p5/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	wordGuess();
+});
+
+function colorGuess() {
+	var wordArray = ["Yellow","YellowGreen"];
+	var randomNumber = Math.floor(Math.random() * wordArray.length);
+	var stripped = wordArray[randomNumber].toLowerCase();
+	console.log(wordArray[randomNumber]);
+	console.log(stripped);
+	$('body').css('background-color', colorArray[randomNumber]);
+	$('#submit').click(function() {
+		var input = $('input').val();
+		var guess = input.toLowerCase();
+		console.log(guess);
+		if (guess == stripped || guess == colorArray[randomNumber]) {
+			$('h1').text(guess + ' is right!');
+			$('input').val('');
+			$('.button p').text('Play Again?');
+			$('#submit').click(function() {
+				location.reload(true);
+			});
+		} else  {
+			$('h1').text(guess + ' is wrong');
+			$('input').val('');
+			$('.button p').text('Play Again?');
+			$('#submit').click(function() {
+				location.reload(true);
+			});
+		}
+	});
+	$(document).keypress(function(e){
+			if(e.which == 13){ //Enter key pressed
+					$('#submit').click();//Trigger button click event
+			}
+	});
+}
