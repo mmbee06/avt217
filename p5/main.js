ad10b96b4064cb09c840e6c32c2725039e64ac4a
mmbee06 <54602972+mmbee06@users.noreply.github.com>
2019-11-21 19:02:23 -0500

    p5

--- p5/main.js
+++ p5/main.js
@@ -1,37 +1,13 @@
-$(document).ready(function() {
-	wordGuess();
+$(document).ready(function(){
+  colorChange ();
+
+function colorChange (){
+	var randomColor = Math.floor(Math.random()*16777215).toString(16);
+	    $(".tile1").css("background-color", '#' + randomColor);
+	});
+	$('.tile1').click(function() {
+		if(clicked){
+	    $('.tile').css('background-color', colorArray[Math.floor(Math.random() * colors.length)]);
 });
 
-function colorGuess() {
-	var wordArray = ["Yellow","YellowGreen"];
-	var randomNumber = Math.floor(Math.random() * wordArray.length);
-	var stripped = wordArray[randomNumber].toLowerCase();
-	console.log(wordArray[randomNumber]);
-	console.log(stripped);
-	$('body').css('background-color', colorArray[randomNumber]);
-	$('#submit').click(function() {
-		var input = $('input').val();
-		var guess = input.toLowerCase();
-		console.log(guess);
-		if (guess == stripped || guess == colorArray[randomNumber]) {
-			$('h1').text(guess + ' is right!');
-			$('input').val('');
-			$('.button p').text('Play Again?');
-			$('#submit').click(function() {
-				location.reload(true);
-			});
-		} else  {
-			$('h1').text(guess + ' is wrong');
-			$('input').val('');
-			$('.button p').text('Play Again?');
-			$('#submit').click(function() {
-				location.reload(true);
-			});
-		}
-	});
-	$(document).keypress(function(e){
-			if(e.which == 13){ //Enter key pressed
-					$('#submit').click();//Trigger button click event
-			}
-	});
-}
+});
