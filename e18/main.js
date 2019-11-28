1662b25de4f5ea8fc6e498514afa3b43b6d5f209
mmbee06 <54602972+mmbee06@users.noreply.github.com>
2019-11-27 19:54:15 -0500

    e18

--- e18/main.js
+++ e18/main.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
+    // run function on initial page load
 	wordGuess();
 });
 
-function colorGuess() {
-	var wordArray = [""];
+function wordGuess() {
+	var wordArray = ["are","acre","ace", "abide", "abate", "adore", "awe", "adhere", "attire", "able", "ache", "acne", "alone", "atone", "age", "aside", "algae", "agree", "apple", "aggrieve", "anodize", "ape"];
 	var randomNumber = Math.floor(Math.random() * wordArray.length);
+	console.log(randomNumber);
 	var stripped = wordArray[randomNumber].toLowerCase();
 	console.log(wordArray[randomNumber]);
 	console.log(stripped);
-	$('body').css('background-color', colorArray[randomNumber]);
+	$('body').css('background-color', wordArray[randomNumber]);
 	$('#submit').click(function() {
 		var input = $('input').val();
 		var guess = input.toLowerCase();
 		console.log(guess);
-		if (guess == stripped || guess == colorArray[randomNumber]) {
+		if (guess == stripped || guess == wordArray[randomNumber]) {
 			$('h1').text(guess + ' is right!');
 			$('input').val('');
 			$('.button p').text('Play Again?');
